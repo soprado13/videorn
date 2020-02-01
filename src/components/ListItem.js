99430b7d0d2b0f9032a1d78c0def1094fad4550b
Sviatoslav V <sviatoslav.vodolaha@gmail.com>
2020-02-02 00:10:55 +0200

    map video list fixes

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import {View, Text, StyleSheet} from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
 
 const ListItem = ({title}) => {
     return (
         <View style={styles.itemWrapper}>
+            <Ionicons name="md-checkmark-circle" size={32} color="#fafafa" />
             <Text style={styles.videoListTitle}>{title}</Text>
         </View>
     )
@@ -13,11 +15,13 @@ export {ListItem}
 
 const styles = StyleSheet.create({
     itemWrapper: {
+        flexDirection: 'row',
         paddingHorizontal: 30,
         paddingVertical: 20,
     },
     videoListTitle: {
         color: '#fafafa',
-        fontSize: 18
+        fontSize: 22,
+        paddingLeft: 20
     }
-})
+});
